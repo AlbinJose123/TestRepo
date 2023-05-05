0c68a02fb732b299fac6ffb42fd29f2b9623ba58
Albin Jose <aj277116@gmail.com>
2023-05-05 23:13:59 +0530

    First commit

---
+++ Fibonacciseries.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+#print("The Fibonacci series : ", end=" ")
+num1 = 0
+num2 = 1
+if num < 1:
+    print(num1, end=" ")
+elif num >= 1:
+    print(num2, end=" ")
+    for x in range(1, num):
+        num3 = num1 + num2
+        num1 = num2
+        num2 = num3
+        print(num3, end=" ")
---
+++ String.py
@@ -0,0 +1,5 @@
+string = input("Enter a Name: ")
+string_1=" "
+for x in string:
+    string_1= x + string_1
+print(string_1)
